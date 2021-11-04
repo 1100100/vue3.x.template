e33a471f2f81a897a622d81b6ffc0a547ffc3641
Owen <315872858@qq.com>
2021-11-04 16:17:22 +0800

    init
    个人vue3预设模板

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,56 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <router-view />
+    <alert :options="dialog"></alert>
+    <loading v-if="showLoading" :msg="msg"></loading>
 </template>
-
+<script>
+import { getCurrentInstance } from "vue";
+import alert from "./components/UI/alert.vue";
+import loading from "./components/UI/loading.vue";
+export default {
+    components: { alert, loading },
+    data() {
+        return {
+            showLoading: false,
+            dialog: {},
+            msg: "加载中...",
+        };
+    },
+    beforeCreate() {
+        const app = getCurrentInstance();
+        app.appContext.config.globalProperties.$showLoading = (_msg) => {
+            if (_msg) this.msg = _msg;
+            this.showLoading = true;
+        };
+        app.appContext.config.globalProperties.$hideLoading = () => {
+            this.showLoading = false;
+        };
+        app.appContext.config.globalProperties.$alert = (arg) => {
+            this.dialog = arg;
+        };
+        app.appContext.config.globalProperties.$http.defaults({
+            timeout: 10000,
+            $400: (err) => {
+                this.$alert({
+                    title: "提示",
+                    content: err.response.data ? err.response.data : err.response.statusText,
+                });
+            },
+            $401: () => {},
+            $finally: () => {
+                this.$hideLoading();
+            },
+        });
+    },
+};
+</script>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    font-size: 14px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import request from "@g1100100/rest.request"
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+app.config.globalProperties.$http = request;
+app.use(router).mount('#app');
